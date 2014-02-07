e0a2a8ed73430f426a7321a397ca6941cbf84038
Steven Spiel <stevenspiel@gmail.com>
2014-02-07 14:42:54 -0600

    Login, logout, and sign-up features complete
    We basically merged the login-logout functionality from http://cl.ly/0v0G1I273W3o
    into our survey project.  We can now sign up, sign in, and sign out.
    
    Basically we the login id from a unique e-mail to a unique username.
    This is why you see a lot of lines changing the e-mail to the username.
    
    We also updated some migrations that we had forgotten to add 's' to, which
    meant the activerecord associations were not working right.
    
    The rest of it is a straight merge from the aformentioned sinatra
    skeleton.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,10 @@
 class User < ActiveRecord::Base
- attr_reader :entered_password
+  attr_reader :entered_password
 
   has_many :participants
   has_many :surveys, :through => :participants
 
-  validates :name, :length => { :minimum => 3, :message => "must be at least 3 characters, fool!" }
+  validates :username, :length => { :minimum => 3, :message => "must be at least 3 characters, fool!" }, uniqueness: true
   validates :entered_password, :length => { :minimum => 6 }
 
   include BCrypt
@@ -19,8 +19,8 @@ class User < ActiveRecord::Base
     self.password_hash = @password
   end
 
-  def self.authenticate(email, password)
-    user = User.find_by_email(email)
+  def self.authenticate(username, password)
+    user = User.find_by_username(username)
     return user if user && (user.password == password)
     nil # either invalid email or wrong password
   end
--- public/js/application.js
+++ public/js/application.js
@@ -4,4 +4,11 @@ $(document).ready(function() {
   // when we try to bind to them
 
   // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+  // send an HTTP DELETE request for the sign-out link
+  $('a#sign-out').on("click", function (e) {
+    e.preventDefault();
+    var request = $.ajax({ url: $(this).attr('href'), type: 'delete' });
+    request.done(function () { window.location = "/"; });
+  });
+
 });
