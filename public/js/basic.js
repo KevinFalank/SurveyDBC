a7f7fc53ae8d195b5b449ce144adb8885d406609
Kevin Falank <kevinf@carolina.rr.com>
2014-02-08 14:40:19 -0600

    Add funtionality to popup the login page
    The signin page is modal for the home page and the take a survey
    page.  Now the submit of the sign in needs to be made AJAX.

--- public/js/basic.js
+++ public/js/basic.js
@@ -13,9 +13,28 @@ jQuery(function ($) {
 	//$('#basic-modal-content').modal();
 
 	// Load dialog on click
-	$('#basic-modal .basic').click(function (e) {
+	$('#header_nav .basic').click(function (e) {
 		$('#basic-modal-content').modal();
 
 		return false;
 	});
+
+	$('.validate_login').click(function (e) {
+		var user_id = $('#logon_user_id').val();
+		if (user_id === "") {
+			e.preventDefault();
+			$('#basic-modal-content').modal();
+		} else {
+			return true;
+		}
+	});
+
+	$('#sign-in').submit(function(){
+	  var data = $(this).serialize();
+	  alert(data)
+	  return false;
+	  
+	});
+
+
 });
