44cc39b0b7f9327e9a77782b6178870946cfdb87
Steven Spiel <stevenspiel@gmail.com>
2014-02-09 11:40:02 -0600

    changed some of the create survey layout

--- app/controllers/survey.rb
+++ app/controllers/survey.rb
@@ -14,10 +14,6 @@ post '/create' do
     redirect '/create'
 end
 
-# Take each element with name that matches var and
-# shovel to an array.
-# Iterate through array to add values to db
-
 get '/surveys/:id' do
   @survey = Survey.find(params[:id])
   @user_id = current_user ? current_user.id : nil
@@ -25,8 +21,6 @@ get '/surveys/:id' do
 end
 
 post '/surveys' do
-  # {"answers"=>{"1"=>"2", "2"=>"8", "3"=>"11", "4"=>"16", "5"=>"25"}, "survey_id"=>"1"}
-  # params.inspect
   @answers = params[:answers]
   @survey_id = params[:survey_id]
   participant = Participant.create(survey_id: @survey_id, user_id: session[:user_id])
@@ -50,7 +44,3 @@ end
 post '/question' do
 
 end
-
-# post '/surveys/:survey_id/survey_stats' do
-
-# end
--- public/js/application.js
+++ public/js/application.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
   var $node = "";
   for(varCount=0;varCount<=startingNo;varCount++){
     var displayCount = varCount+1;
-    $node += '<p><label for="var'+displayCount+'"></label><input type="text" placeholder="Answer" name="var'+displayCount+'" id="var'+displayCount+'"><span class="removeVar"> Remove</span></p>';
+    $node += '<p><label for="var'+displayCount+'"></label><input type="text" placeholder="Answer" name="var'+displayCount+'" id="var'+displayCount+'"><button class="removeVar"> Remove</button></p>';
   }
   $('.answers').prepend($node);
 
@@ -14,7 +14,7 @@ $(document).ready(function() {
 
   $('#addVar').on('click', function(){
     varCount++;
-    $node = '<p><label for="var'+varCount+'"></label><input placeholder="Answer" type="text" name="var'+varCount+'" id="var'+varCount+'"><span class="removeVar"> Remove</span></p>';
+    $node = '<p><label for="var'+varCount+'"></label><input placeholder="Answer" type="text" name="var'+varCount+'" id="var'+varCount+'"><button class="removeVar"> Remove</button></p>';
     $(this).parent().before($node);
   });
 
