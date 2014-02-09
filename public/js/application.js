0151da346b7735c3e4eee4cb151d55ce72f38d26
Jacky Sum <jsum528@icloud.com>
2014-02-09 15:54:14 -0600

    fixed add answer bug on create survey page

--- public/js/application.js
+++ public/js/application.js
@@ -19,7 +19,7 @@ $(document).ready(function() {
   });
 
   // send an HTTP DELETE request for the sign-out link
-  $('a#sign-out').on("click", function (e) {
+  $('a#sign-out').submit(function (e) {
     e.preventDefault();
     var request = $.ajax({ url: $(this).attr('href'), type: 'delete' });
     request.done(function () {
