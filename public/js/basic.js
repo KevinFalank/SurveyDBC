51cc2eca4afaac72e56a6af8d19da3fee6f62009
Kevin Falank <kevinf@carolina.rr.com>
2014-02-08 18:11:53 -0600

    corrected user login error in modal login form and reapplied click event handling to sign-out link when needed

--- public/js/basic.js
+++ public/js/basic.js
@@ -39,28 +39,41 @@ jQuery(function ($) {
 		$.post( "/sessions", data, function (e) {
 			//console.log(e);
 
-			var loggedOnUserNav = "<li><a id='acct-mgmt' href='/users/" + e + "/account'>Account Management</a></li> <li><a id='create-survey' href='/create'>Create Survey</a></li> <li><a id='sign-out' href='/sessions/" + e + "'>Sign Out</a></li>"
-      //console.log(loggedOnUserNav);
-      $('#logged_on_user').html(loggedOnUserNav);
+			if (e === "0") {
+				$('#basic-modal-content .errors').html('Invalid user name or password.<br/><br/>')
+			} else {
+
+				var loggedOnUserNav = "<li><a id='acct-mgmt' href='/users/" + e + "/account'>Account Management</a></li> <li><a id='create-survey' href='/create'>Create Survey</a></li> <li><a id='sign-out' href='/sessions/" + e + "'>Sign Out</a></li>"
+	      //console.log(loggedOnUserNav);
+	      $('#logged_on_user').html(loggedOnUserNav);
+
+	      // reapply the click event handler to the sign-out link
+				$('a#sign-out').on("click", function (e) {
+				    e.preventDefault();
+				    var request = $.ajax({ url: $(this).attr('href'), type: 'delete' });
+				    request.done(function () {
+				      window.location = "/";
+				    });
+				  });
 
 
+				if ($('#display_survey')) {
+					$('#logon_user_id').val(e);
+				}
+			  if (survey_data_submitted === true) {
+			  	console.log('survey data submitted!');
+			  	$.post("/surveys", survey_form_data, function() {
+			  		// alert ("survey submitted");
+			  		survey_form_data = ""
+			  		survey_data_submitted = false;
+			  		window.location.href = '/'
 
-			if ($('#display_survey')) {
-				$('#logon_user_id').val(e);
+			  	});
+			  } else {
+			  	// window.location.href = '/'
+			  	$.modal.close();
+			  }
 			}
-		  if (survey_data_submitted === true) {
-		  	console.log('survey data submitted!');
-		  	$.post("/surveys", survey_form_data, function() {
-		  		// alert ("survey submitted");
-		  		survey_form_data = ""
-		  		survey_data_submitted = false;
-		  		window.location.href = '/'
-
-		  	});
-		  } else {
-		  	// window.location.href = '/'
-		  	$.modal.close();
-		  }
 		});
 
 	});
