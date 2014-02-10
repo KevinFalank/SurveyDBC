74bfafda123a0187859eb713854e253eec9575a8
Tara Roys <tlroys@gmail.com>
2014-02-09 20:37:45 -0600

    Wrote a test for Survey has many participants relation
    It works. Yay.

--- spec/survey_spec.rb
+++ spec/survey_spec.rb
@@ -6,7 +6,14 @@ describe Survey do
       it "belongs to a user" do
         animal_sounds = Survey.find_by_title("Animal Sounds")
         expect(animal_sounds.user.username).to eql("jacky")
+      end
+
+      it "has many participants" do 
+        animal_sounds = Survey.find_by_title("Animal Sounds")
+        expect(animal_sounds.participants.length).not_to eql(0); 
       end 
+
+      
     end
   end
 
