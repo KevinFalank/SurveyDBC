c9d72a2805dada43ce0d2853d74ff37df35abdc2
Tara Roys <tlroys@gmail.com>
2014-02-09 22:18:40 -0600

    Testing Result belongs to Answer
    Wrote a test to see if the Activerecord Association Result belongs to Answer is in fact working.  It is.

--- spec/result_spec.rb
+++ spec/result_spec.rb
@@ -1,10 +1,14 @@
 require 'spec_helper'
 
 describe Result do
- context "Testing Activerecord Associations" do
-  it "belongs to participant" do
-    jackys_first_answer = Result.where('id =?', 1).first
-    expect(jackys_first_answer.participant.user.username).to eql('jacky') 
+  context "Testing Activerecord Associations" do
+    it "belongs to participant" do
+      jackys_first_answer = Result.where('id =?', 1).first
+      expect(jackys_first_answer.participant.user.username).to eql('jacky') 
+    end  
+    it "belongs to answer" do 
+     jackys_first_answer = Result.where('id =?', 1).first
+     expect(jackys_first_answer.answer.text).to eql('Wa-pa-pa-pa-pa-pa-pa-pow!')
+    end
   end  
- end 
 end
