206a4bc8fb0c8ce2b1aa534eef5f8689febca78b
Tara Roys <tlroys@gmail.com>
2014-02-09 21:32:32 -0600

    Test: Question has many Answers
    Wrote a test to see if the Activerecord associtation question has many answers was working. It was.

--- spec/question_spec.rb
+++ spec/question_spec.rb
@@ -6,5 +6,12 @@ describe Question do
       fox = Question.all.first
       expect(fox.survey.title).to eql("Animal Sounds")
     end
+
+    it "has many answers" do 
+      fox = Question.all.first
+      expect(fox.answers[0].text).to eql('Wa-pa-pa-pa-pa-pa-pa-pow!')
+      expect(fox.answers[1].text).to eql('Gering-ding-ding-ding-dingeringeding!')
+      expect(fox.answers[2].text).to eql('Hatee-hatee-hatee-ho!')
+    end 
   end 
 end
