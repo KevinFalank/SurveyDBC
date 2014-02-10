70f281d8410167c70c87501fb2d796726bb9ea11
Tara Roys <tlroys@gmail.com>
2014-02-09 21:07:50 -0600

    Test: Praticipant belongs to survey
    It passes. A participant indeed belongs to a survey

--- spec/participant_spec.rb
+++ spec/participant_spec.rb
@@ -5,7 +5,12 @@ describe Participant do
     it "belongs to user" do
       jacky = Participant.find_by_id(1)
       expect(jacky.user.username).to eql("jacky")
-
     end 
+
+    it "belongs to survey" do 
+      jacky = Participant.find_by_id(1)
+      expect(jacky.survey.title).to eql("Animal Sounds")
+    end 
+    
   end 
 end
