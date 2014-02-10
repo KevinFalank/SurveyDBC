8069046274b62be579beb5957baf8b9324bf3106
Tara Roys <tlroys@gmail.com>
2014-02-09 18:42:53 -0600

    Added User password validation test
    Added a test to see if the user gave a password that was at least six characters long.
    
    Leassons learned: activerecord gives this fascinating...I'm not sure if it's a method or parameter. But it's called errors. So when activerecord is not successful in sticking something in the database, it still keeps the ruby object around, and fills it full of error messages so that a programmer has a fighting chance of figuring out what is wrong.  Activerecord errors is like an english teacher sending back a paper full of red ink and instructions to do it over.

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -70,6 +70,11 @@ describe User do
         user = User.create(username: "ca")
         expect(user.errors.messages[:username][0]).to eql("must be at least 3 characters, fool!")
       end
+      
+      it "Password must be at least six characters long" do
+        user = User.create(password: "ca")
+        expect(user.errors.messages[:entered_password][0]).to eql("is too short (minimum is 6 characters)")
+      end
     end
   end
 
